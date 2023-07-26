17d182b679887e4b69a6f97264955ca0869aa3d0
makachikava <chikavamakanaka@gmail.com>
2023-07-25 21:32:48 -0700

    create vue app and began creating template

---
+++ todolist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>To Do's</h1>
+  <input type="text">
+  <button>submit</button>
+  <allTodos/>
+</template>
+
+<script>
+import allTodos from './components/allTodos.vue';
+
+export default {
+  name: 'App',
+  components: {
+    allTodos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todolist/src/components/allTodos.vue
@@ -0,0 +1,36 @@
+<template>
+<p>list of allTodos</p>
+</template>
+
+<script>
+export default {
+data() {
+return{
+    allTodos: [],
+
+    todo:{
+        todo:"",
+        check: false
+    }
+    
+}
+},
+
+methods: {
+
+refreshData(){
+
+},
+
+createTodo(){
+
+},
+
+deleteTodo(){
+
+}
+
+
+}
+}
+</script>
