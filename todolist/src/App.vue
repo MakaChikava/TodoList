f38f803c951c037b21ba465d5c977230613c5b0a
makachikava <chikavamakanaka@gmail.com>
2023-07-26 00:40:04 -0700

    created the create function and get function

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,18 +1,71 @@
 <template>
+<div>
   <h1>To Do's</h1>
-  <input type="text">
-  <button>submit</button>
-  <allTodos/>
+  <input type="text" v-model="todoList.todo">
+  <button @click=" createTodo() ">submit</button>
+  <h3>list of allTodos</h3>
+
+<p v-for="todo in allTodos" :key="todo._id"> 
+{{ todo.todo }}
+</p>
+</div>
 </template>
 
 <script>
-import allTodos from './components/allTodos.vue';
 
+import axios from 'axios'
 export default {
+
   name: 'App',
-  components: {
-    allTodos
-  }
+
+  data(){
+    
+    return{
+
+      allTodos:[],
+
+      todoList:{
+        todo:"",
+        check: false
+    }
+
+  };
+  },
+  methods: {
+
+    refreshData(){
+axios
+    .get('http://localhost:8000')
+    .then((res)=>{
+        console.log(res.data)
+        this.allTodos = res.data
+    })
+},
+
+createTodo(){
+axios
+  .post('http://localhost:8000/todo', {
+    todo: this.todoList.todo,
+    check: this.todoList.check
+})
+  .then((res)=>{
+    console.log(res.data)
+    this.refreshData
+  })
+},
+
+deleteTodo(){
+
+}
+
+
+},
+
+mounted:function()
+    {
+    this.refreshData();
+    }
+
 }
 </script>
 
