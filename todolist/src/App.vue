2fb7946181a7907d81698616a4b518cf4715a061
makachikava <chikavamakanaka@gmail.com>
2023-07-27 16:40:36 -0700

    added strikethrough toggle switch

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,13 +1,24 @@
 <template>
 <div>
+
   <h1>To Do's</h1>
   <input type="text" v-model="todoList.todo">
   <button @click=" createTodo() ">submit</button>
+
   <h3>list of allTodos</h3>
 
-<p v-for="todo in allTodos" :key="todo._id" > 
-{{ todo.todo }} <button @click="deleteTodo(todo._id)">delete</button>
-</p>
+<div v-for="todo in allTodos" :key="todo._id">
+  <p v-if="todo.check" @click="todo.check = !todo.check"> 
+  {{ todo.todo }} 
+  </p>
+  
+
+  <p class="change" v-else @click="todo.check = !todo.check">
+    {{ todo.todo }} 
+  </p><button @click="deleteTodo(todo._id)">delete</button>
+  
+</div>
+
 </div>
 </template>
 
@@ -22,11 +33,13 @@ export default {
     
     return{
 
+      awesome: false,
+
       allTodos:[],
 
       todoList:{
         todo:"",
-        check: false
+        check: true
     }
 
   };
@@ -59,9 +72,14 @@ axios
   .delete(`http://localhost:8000/todo/${id}`)
   .then(()=>{
     this.refreshData()
+    
   })
-}
+},
 
+// switch list item from true to false vice versa
+handleToggle(){
+
+}
 
 },
 
@@ -81,5 +99,8 @@ mounted:function()
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.change{
+  text-decoration: line-through;
 }
 </style>
